38567358c67e0e86fd67c3737d66287822a02fe8
Gregory Kuzin <g.kuzin@dssl.ru>
2024-04-23 18:05:48 +0300

    06lab

--- server.js
+++ server.js
@@ -1,23 +1,39 @@
 const express = require('express');
+const {MongoClient} = require('mongodb');
 
 const app = express();
 const port = 8000;
 
 app.use(express.static('public'))
 
-app.get('/', function(req, res) {
-    const data = {};
+app.get('/tasks', async function(req, res) {
+    const client = new MongoClient('mongodb://127.0.0.1');
+    await client.connect();
+    const db = client.db('todoapp');
+    const collection = db.collection('tasks');
+    const data = await collection.find({}).toArray();
     res.send(data);
 });
-app.get('/cart', function(req, res) {
-    console.log(req.query);
-    const data = {};
-    for (item in req.query) {
-        data[item] = 0.5 * req.query[item]
-    }
 
-    res.send(data);
+app.get('/tasks/:id', function(req, res) {
 });
+
+app.post('/tasks', async function(req, res) {
+    const task = {};
+    const client = new MongoClient('mongodb://127.0.0.1');
+    await client.connect();
+    const db = client.db('todoapp');
+    const collection = db.collection('tasks');
+    await collection.insertOne(task);
+    res.send(task);
+});
+
+app.patch('/tasks/:id', function(req, res) {
+});
+
+app.delete('/tasks/:id', function(req, res) {
+});
+
 app.listen(port, function() {
     console.log('Server is started!');
 });
