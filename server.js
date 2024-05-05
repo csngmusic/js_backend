75305f605cd6897cba0dc2823bba94c90e79dce3
Gregory Kuzin <g.kuzin@dssl.ru>
2024-05-05 11:02:02 +0300

    10tutor

--- server.js
+++ server.js
@@ -11,35 +11,36 @@ async function getDbCollection(dbAddress, dbName, dbCollectionName) {
     await client.connect();
     const db = client.db(dbName);
     const collection = db.collection(dbCollectionName);
+    return db.collection(dbCollectionName);
 }
 app.get('/tasks', async function(req, res) {
-    const collection = getDbCollection('mongodb://127.0.0.1', 'todoapp', 'tasks');
+    const collection = await getDbCollection('mongodb://127.0.0.1', 'todoapp', 'tasks');
     const data = await collection.find({}).toArray();
     res.send(data);
 });
 
 app.get('/tasks/:id', async function(req, res) {
-    const collection = getDbCollection('mongodb://127.0.0.1', 'todoapp', 'tasks');
+    const collection = await getDbCollection('mongodb://127.0.0.1', 'todoapp', 'tasks');
     const data = await collection.findOne({_id: new ObjectId(req.params.id)});
     res.send(data);
 });
 
 app.post('/tasks', async function(req, res) {
     const task = {...req.body, done: false};    
-    const collection = getDbCollection('mongodb://127.0.0.1', 'todoapp', 'tasks');
+    const collection = await getDbCollection('mongodb://127.0.0.1', 'todoapp', 'tasks');
     await collection.insertOne(task);
     res.send(task);
 });
 
 app.patch('/tasks/:id', async function(req, res) {
-    const collection = getDbCollection('mongodb://127.0.0.1', 'todoapp', 'tasks');
+    const collection = await getDbCollection('mongodb://127.0.0.1', 'todoapp', 'tasks');
     await collection.updateOne({_id: new ObjectId(req.params.id)}, 
                                 {'$set': req.body});
     res.send({});
 });
 
 app.delete('/tasks/:id', async function(req, res) {
-    const collection = getDbCollection('mongodb://127.0.0.1', 'todoapp', 'tasks');
+    const collection = await getDbCollection('mongodb://127.0.0.1', 'todoapp', 'tasks');
     await collection.deleteOne({_id: new ObjectId(req.params.id)});
     res.send({});
 });
